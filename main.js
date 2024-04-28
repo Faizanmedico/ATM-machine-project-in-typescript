55c3f15f6b03defa7b6e66caa2b8fc4c1f295437
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-04-28 12:21:10 +0500

    Add files via upload
    ATM Machine Project | TypeScript , Node.js & Inquirer | Governor IT Course | Npx command | Submit Assignment

---
+++ main.js
@@ -0,0 +1,8 @@
+const list = ['a', 'b', 'c', 'd', 'e'];
+let i = 0;
+while (i < list.length) {
+    console.log(list[i]); //value
+    console.log(i); //index
+    i = i + 1;
+}
+export {};
---
+++ main.ts
@@ -0,0 +1,7 @@
+const list = ['a', 'b', 'c', 'd', 'e']
+let i = 0
+while (i < list.length) {
+  console.log(list[i]) //value
+  console.log(i) //index
+  i = i + 1
+}
